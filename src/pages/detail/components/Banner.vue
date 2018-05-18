5827a683b978e768849bd831f9b50505367c5fb6
sdsno <89716898@qq.com>
2018-05-18 16:03:14 +0800

    ssss

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -7,15 +7,18 @@
         <div class="banner-number"><span class="iconfont banner-icon">&#xe692; </span> {{this.bannerImgs.length}}</div>
       </div>
     </div>
-    <common-gallary
-      :imgs=" bannerImgs"
-      v-show="showGallary"
-      @close="handleGallaryClose"
-></common-gallary>
+    <fade-animation>
+      <common-gallary
+        :imgs=" bannerImgs"
+        v-show="showGallary"
+        @close="handleGallaryClose"
+      ></common-gallary>
+    </fade-animation>
   </div>
 </template>
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'Banner',
   props: {
@@ -37,7 +40,8 @@ export default {
     }
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   }
 }
 </script>
